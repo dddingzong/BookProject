4685b52cb5a58a485431ba9f407bd8352cb02eac
USER <cji513@naver.com>
2024-06-25 19:57:48 +0900

    p.151

--- src/main/java/com/bookproject/springboot/service/posts/PostsService.java
+++ src/main/java/com/bookproject/springboot/service/posts/PostsService.java
@@ -1,11 +1,17 @@
 package com.bookproject.springboot.service.posts;
 
+import com.bookproject.springboot.domain.posts.Posts;
 import com.bookproject.springboot.domain.posts.PostsRepository;
+import com.bookproject.springboot.web.dto.PostsListResponseDto;
+import com.bookproject.springboot.web.dto.PostsResponseDto;
 import com.bookproject.springboot.web.dto.PostsSaveRequestDto;
+import com.bookproject.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -16,6 +22,29 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
 
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById (Long id) {
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
+
+        return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
