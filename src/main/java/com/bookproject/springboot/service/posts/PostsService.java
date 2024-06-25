acb98781e1de6a6ed531af5b5ef5d05e2b3cbd74
USER <cji513@naver.com>
2024-06-25 18:47:09 +0900

    p.111

---
+++ src/main/java/com/bookproject/springboot/service/posts/PostsService.java
@@ -0,0 +1,21 @@
+package com.bookproject.springboot.service.posts;
+
+import com.bookproject.springboot.domain.posts.PostsRepository;
+import com.bookproject.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+
+    }
+}
